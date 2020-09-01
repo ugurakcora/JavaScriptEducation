ca18066ef8d66c3b928c607c0c95079fe9abcd0d
ugur.akcora <ugur.akcora@pixelplus.net>
2020-09-01 17:02:19 +0300

    for

---
+++ Temel/ders-4/script.js
@@ -0,0 +1,16 @@
+// loops
+
+// for
+
+for (let i = 0; i < 10; i++) {
+  if (i == 3) {
+    console.log("En sevdiğim rakam: " + i);
+    continue;
+  }
+
+  if (i == 6) {
+    console.log("En sevmediğim rakam: " + i);
+    break;
+  }
+  console.log(i);
+}
