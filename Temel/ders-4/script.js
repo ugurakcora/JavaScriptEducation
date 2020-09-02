7acef0cc3d1cb5ce67563b9858c6436b94d50996
ugur.akcora <ugur.akcora@pixelplus.net>
2020-09-02 11:37:54 +0300

    donguler

--- Temel/ders-4/script.js
+++ Temel/ders-4/script.js
@@ -15,6 +15,41 @@ for (let i = 0; i < 10; i++) {
   console.log(i);
 }
 
-
 // while
+let i = 0;
+while (i < 10) {
+  console.log(i);
+  i++;
+}
 
+// do while
+let a = 0;
+do {
+  console.log(a);
+  a++;
+} while (a < 10);
+
+let toplam = 0;
+for (i = 10; i > 10; i--) {
+  console.log(i);
+  toplam += i;
+}
+console.log(toplam);
+
+let sonuc = 1;
+for (i = 0; i < 10; i++) {
+  if (i % 2 == 0) {
+    sonuc *= i;
+  }
+}
+console.log(sonuc);
+
+let val = "\n";
+for (let i = 0; i < 10; i++) {
+  for (let j = 0; j < 10; j++) {
+    console.log(`i : ${i} j : ${j}`);
+    val += "* ";
+  }
+  val += "\n";
+}
+console.log(val);
